241a3c52fcb156893c1dfa60dd801756c0cdc8c1
BergeDios <santiago.goyret@gmail.com>
2022-01-11 09:34:17 -0800

    fix monty

--- monty.h
+++ monty.h
@@ -4,7 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <fcntl.h>
+#include <unistd.h>
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
